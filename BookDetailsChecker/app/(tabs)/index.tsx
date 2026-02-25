2b860f1bb0f2b140f20be82214cebecbad349877
Azar <azar.wirth@outlook.com>
2026-02-25 11:40:11 +0100

    changed formatting to be used as main index

--- BookDetailsChecker/app/(tabs)/index.tsx
+++ BookDetailsChecker/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#000", // optional, damit man weiß sieht
+    backgroundColor: "#000",
   },
   title: {
     color: "white",
