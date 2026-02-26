854628ea93d7a7d95d7ce97944d0546dc2f49b90
Moreno Ramanti <moreno.ramanti@noseryoung.com>
2026-02-26 13:02:37 +0100

    Added interceptor to api.ts for login authentification

--- BookDetailsChecker/services/api.ts
+++ BookDetailsChecker/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 export const api = axios.create({
   baseURL: "http://10.0.2.2:3030",
@@ -6,3 +7,22 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  async (config: any) => {
+    try {
+      const token = await SecureStore.getItemAsync("userToken");
+      
+      if (token) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error("Fehler beim Laden des Tokens:", error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
