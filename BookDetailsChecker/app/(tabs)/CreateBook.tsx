58023f0832f3262e0cf321bfc0bfae60e3da875f
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2026-02-27 12:00:16 +0100

    add placeholder to create function

--- BookDetailsChecker/app/(tabs)/CreateBook.tsx
+++ BookDetailsChecker/app/(tabs)/CreateBook.tsx
@@ -151,13 +151,24 @@ const CreateBook = () => {
         <Text style={styles.header}>Create new Book</Text>
         <Text style={styles.sectionTitle}>Book Details</Text>
 
-        <FormInput label="Title" value={title} onChangeText={setTitle} />
-        <FormInput label="ISBN13" value={isbn13} onChangeText={setIsbn13} />
+        <FormInput
+          label="Title"
+          value={title}
+          onChangeText={setTitle}
+          placeholder="e.g. The Metamorphosis"
+        />
+        <FormInput
+          label="ISBN13"
+          value={isbn13}
+          onChangeText={setIsbn13}
+          placeholder="e.g. 1234567892"
+        />
         <FormInput
           label="Number of Pages"
           value={pages}
           onChangeText={setPages}
           keyboardType="numeric"
+          placeholder="e.g. 232"
         />
 
         <Text style={styles.label}>Publication Date</Text>
@@ -190,18 +201,26 @@ const CreateBook = () => {
           label="Publisher"
           value={publisherName}
           onChangeText={setPublisherName}
+          placeholder="e.g. 1st Book Library"
         />
         <FormInput
           label="Author"
           value={authorName}
           onChangeText={setAuthorName}
+          placeholder="e.g. Franz Kafka"
         />
         <FormInput
           label="Language"
           value={languageName}
           onChangeText={setLanguageName}
+          placeholder="e.g. English"
         />
-        <FormInput label="Image url" value={image} onChangeText={setImage} />
+        <FormInput
+          label="Image url"
+          value={image}
+          onChangeText={setImage}
+          placeholder="https://your-image-url.com/image.jpg"
+        />
 
         <FormActions onSave={handleSave} onCancel={handleCancel} />
       </ScrollView>
@@ -209,7 +228,13 @@ const CreateBook = () => {
   );
 };
 
-const FormInput = ({ label, value, onChangeText, keyboardType }: any) => (
+const FormInput = ({
+  label,
+  value,
+  onChangeText,
+  keyboardType,
+  placeholder,
+}: any) => (
   <View style={styles.inputGroup}>
     <Text style={styles.label}>{label}</Text>
     <TextInput
@@ -217,6 +242,7 @@ const FormInput = ({ label, value, onChangeText, keyboardType }: any) => (
       value={value}
       onChangeText={onChangeText}
       keyboardType={keyboardType}
+      placeholder={placeholder}
       placeholderTextColor={Colors.light.textField}
     />
   </View>
